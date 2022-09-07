9cf4990d92631f703942113eacf685c190be385f
pav <me@pavmail.ca>
2022-09-07 18:13:49 -0400

    [ss-amplifer] - small signal amplifer design in ADS

---
+++ small_signal_amplifier/ADS/LET564_Models/workspace.ads
@@ -0,0 +1,32 @@
+<ADSWorkspace Revision="21" Version="100">
+    <Workspace Name="">
+        <Library Name="ads_standard_layers" />
+        <Library Name="adstechlib" />
+        <Library Name="ads_schematic_layers" />
+        <Library Name="empro_standard_layers" />
+        <Library Name="ads_standard_layers_ic" />
+        <Library Name="ads_schematic_layers_ic" />
+        <Library Name="ads_schematic_ports_ic" />
+        <Library Name="ads_rflib" />
+        <Library Name="ads_sources" />
+        <Library Name="ads_simulation" />
+        <Library Name="ads_tlines" />
+        <Library Name="ads_bondwires" />
+        <Library Name="ads_datacmps" />
+        <Library Name="ads_behavioral" />
+        <Library Name="ads_textfonts" />
+        <Library Name="ads_common_cmps" />
+        <Library Name="ads_designs" />
+        <Library Name="ads_verification_test_bench" />
+        <Library Name="LET564_Models_lib" />
+        <Preferences Name="layout.prf" />
+        <Preferences Name="schematic.prf" />
+        <LibraryDefs Name="lib.defs" />
+        <ConfigFile Name="de_sim.cfg" />
+        <ConfigFile Name="hpeesofsim.cfg" />
+        <Cell Name="LET564_Models_lib:SMA_contact" />
+        <Log Name="search_history.log" />
+        <Cell Name="LET564_Models_lib:ATF55143_Final" />
+        <Cell Name="LET564_Models_lib:ATF-55143_footprint" />
+    </Workspace>
+</ADSWorkspace>
